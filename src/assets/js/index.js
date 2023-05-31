2578fa3e85af5fc413da84f1132cdfc4247dc084
Mochalov-Hunter <mochalov-hunter@mail.ru>
2023-05-31 13:31:51 +0300

    обновления

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -2,3 +2,17 @@ import "../../index.html";
 import "../style/index.scss";
 
 // npm import
+
+// install Swiper
+import Swiper, { Navigation, Pagination } from 'swiper'
+
+const swiper = new Swiper('.main-slider', {
+    modules: [Navigation, Pagination],
+    loop: true,
+    autoplay: {
+        delay: 5000,
+    },
+    pagination: {
+        el: '.main-slider__pagination',
+    },
+  });
